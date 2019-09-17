03a9a5907390aa4c8ee857f132d1a95d31453d12
Faris Nour <farisnour93@gmail.com>
2019-09-16 22:14:29 -0400

    added getData Function

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import math
+import matplotlib.pyplot as plt
+import random
+
+import numpy as np
+import tensorflow as tf
+
+
+def getData(N, variance):
+    sigma = math.sqrt(variance)
+    z = np.random.normal(0, sigma, N)
+
+    x = np.random.random_sample(N)
+    y = np.array([F(i) for i in x]) + z
+
+    return x, y
+
+
+def F(x):
+    return math.cos(2 * math.pi * x)
+
+
+x, y = getData(500, 0.015)
+plt.scatter(x, y)
+plt.show()
 
