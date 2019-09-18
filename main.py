a59e20d84f47e53c4b47cb2fbad5a5ca5eef01cb
Faris Nour <farisnour93@gmail.com>
2019-09-17 21:47:39 -0400

    Added getMSE function

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import numpy as np
 import tensorflow as tf
 
 
+def F(x):
+    return math.cos(2 * math.pi * x)
+
+
 def getData(N, variance):
     sigma = math.sqrt(variance)
     z = np.random.normal(0, sigma, N)
@@ -16,11 +20,23 @@ def getData(N, variance):
     return x, y
 
 
-def F(x):
-    return math.cos(2 * math.pi * x)
+def getMSE(x, y, parameters):
+    d = len(parameters)
+    N = len(x)
+
+    sum = 0
+    for i in range(N):
+        result = 0
+        for j in range(d):
+            result = result + parameters[j] * math.pow(x[i], j)
+        sum += math.pow(y[i] - result, 2)
+    return sum / N
 
 
 x, y = getData(500, 0.015)
-plt.scatter(x, y)
-plt.show()
+print('x:', x)
+print('y:', y)
+print('getMSE:', getMSE(x, y, np.array([1, 3, 2, 0.3])))
+# plt.scatter(x, y)
+# plt.show()
 
